116a869d9c6f8b67b6f1f4d58ca1016087a9c314
MillieOnWays <l.udalova90@gmail.com>
2022-02-08 10:35:21 +0100

    NavBar styles+search bar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "./styles.css";
 import NavBarItem from "./NavBarItem";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
 
 export default function NavBar() {
   return (
     <div className="container">
-      <div className="input-container"></div>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Search.."
+          style={{
+            width: "15rem",
+            background: "#F2F1F9",
+            border: "none",
+            padding: "0.5rem",
+            marginTop: "0.6rem",
+          }}
+        />
+      </div>
       <div className="link-container">
         <NavBarItem path="/" linkText="HOME"></NavBarItem>
         <NavBarItem path="/products" linkText="PRODUCTS"></NavBarItem>
