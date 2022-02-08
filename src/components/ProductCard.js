eed8bc62b7eeded625f9037824118f30ec3524c4
MillieOnWays <l.udalova90@gmail.com>
2022-02-08 17:12:17 +0100

    Details page created

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Container, Row, Card, Button } from "react-bootstrap";
+import { Col, Row, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function ProductCard(props) {
--- src/pages/DetailsPage/index.js
+++ src/pages/DetailsPage/index.js
@@ -1,3 +1,61 @@
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchProductDetails } from "../../store/product/actions";
+import { selectProductDetails } from "../../store/product/selectors";
+import { Col, Row, Button, Card } from "react-bootstrap";
+import { AiOutlineHeart } from "react-icons/ai";
+import {
+  TiSocialFacebookCircular,
+  TiSocialInstagram,
+  TiSocialTwitterCircular,
+} from "react-icons/ti";
+
 export default function DetailsPage() {
-  return <h1>Details Page</h1>;
+  const dispatch = useDispatch();
+  const params = useParams();
+  // console.log("params", params);
+  const details = useSelector(selectProductDetails);
+  // console.log("details", details);
+
+  useEffect(() => {
+    dispatch(fetchProductDetails(params.id));
+  }, [dispatch, params.id]);
+
+  if (!details) return <h1>Loading</h1>;
+
+  return (
+    <Card>
+      <Card.Body>
+        <Row>
+          <Col>
+            <Card.Img
+              src={details.mainImage}
+              alt="product-img"
+              style={{ width: 250, height: 250 }}
+            />
+          </Col>
+          <Col>
+            <h1>{details.title}</h1>
+            <Col>
+              {details.rating} <Button> Add Review </Button>
+            </Col>
+            <p>€{details.price}</p>
+            <p>{details.description}</p>
+            <Button> Add to Cart </Button>{" "}
+            <Button>
+              {" "}
+              <AiOutlineHeart />
+              Favorite
+            </Button>
+            <p>Categories: {details.category.title}</p>
+            <p>
+              Share: <TiSocialFacebookCircular /> <TiSocialInstagram />{" "}
+              <TiSocialTwitterCircular />{" "}
+            </p>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
 }
